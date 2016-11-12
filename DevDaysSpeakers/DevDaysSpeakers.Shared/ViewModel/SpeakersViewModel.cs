ff3f71821c145635de2ad57acfd39b024486f49f
Miguel Moreno G <miguel.morenog@outlook.es>
2016-11-12 18:33:26 +0100

    Added my bio on the list of speakers.

--- DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -56,14 +56,26 @@ namespace DevDaysSpeakers.ViewModel
             try
             {
                 IsBusy = true;
+                using (var client = new HttpClient())
+                {
+                    //grab json from server
+                    var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+                    var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
 
-                var service = DependencyService.Get<AzureService>();
-                var items = await service.GetSpeakers();
+                    Speaker miguelmg = new Speaker();
+                    miguelmg.Id = "100";
+                    miguelmg.Name = "Miguel Moreno";
+                    miguelmg.Description = "I work at Viajes Para Ti S.L.U. I work on applications departement and I do the BuscoUnChollo application for Windows Platform. Also I work on web department as a backend developer of my company portals on PHP.";
+                    miguelmg.Website = "https://github.com/miguelmg";
+                    miguelmg.Title = "MiguelMG";
+                    miguelmg.Avatar = "https://avatars1.githubusercontent.com/u/3147742?v=3&amp;s=460";
 
-                Speakers.Clear();
-                foreach (var item in items)
-                    Speakers.Add(item);
+                    //Speakers.clear();
+                    foreach (var item in items)
+                        Speakers.Add(item);
 
+                    Speakers.Add(miguelmg);
+                }
             }
             catch (Exception ex)
             {
