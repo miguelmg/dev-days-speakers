47b53bb96619f3dc074f4fb6d15b19f51aba346a
Miguel Moreno G <miguel.morenog@outlook.es>
2016-11-12 18:16:06 +0100

    Now work with azure services!

--- DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -57,17 +57,12 @@ namespace DevDaysSpeakers.ViewModel
             {
                 IsBusy = true;
 
-                using (var client = new HttpClient())
-                {
-                    //grab json from server
-                    var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
-                    var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+                var service = DependencyService.Get<AzureService>();
+                var items = await service.GetSpeakers();
 
-                    //Speakers.clear();
-                    foreach (var item in items)
-                        Speakers.Add(item);
-
-                }
+                Speakers.Clear();
+                foreach (var item in items)
+                    Speakers.Add(item);
 
             }
             catch (Exception ex)
